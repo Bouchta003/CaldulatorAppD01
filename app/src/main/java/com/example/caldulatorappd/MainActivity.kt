a3b9796042160856b704d1df2cdf2f8dd7bb06f7
Ali Bennouna Louridi <alibennouna003@gmail.com>
2024-01-27 12:16:06 +0100

    Added Text and some button behaviour

--- app/src/main/java/com/example/caldulatorappd/MainActivity.kt
+++ app/src/main/java/com/example/caldulatorappd/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.caldulatorappd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonU = findViewById<Button>(R.id.buttonU)
+        val buttonE = findViewById<Button>(R.id.buttonE)
+        val resultTxt = findViewById<TextView>(R.id.outputTxt)
+
+        val buttons = listOf(buttonU, buttonE /* Add more buttons to the list */)
+
+        for (button in buttons) {
+            button.setOnClickListener {
+                resultTxt.text = button.text
+            }
+        }
     }
 }
