de58628ebae841f7a1b029ffdc526e55fdc73be0
Ali Bennouna Louridi <alibennouna003@gmail.com>
2024-01-27 13:07:28 +0100

    Changed some Colors, better logic between steps and result ->Needs the special colors ->Needs the icons for some buttons ->Needs the calculus logic for the Math

--- app/src/main/java/com/example/caldulatorappd/MainActivity.kt
+++ app/src/main/java/com/example/caldulatorappd/MainActivity.kt
@@ -6,18 +6,52 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private val stringBuilder = StringBuilder()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonU = findViewById<Button>(R.id.buttonU)
-        val buttonE = findViewById<Button>(R.id.buttonE)
-        val resultTxt = findViewById<TextView>(R.id.outputTxt)
-
-        val buttons = listOf(buttonU, buttonE /* Add more buttons to the list */)
+        val stepsTxt = findViewById<TextView>(R.id.stepsTxt)
+        val outputTxt = findViewById<TextView>(R.id.outputTxt)
+        val buttons = listOf(
+            findViewById<Button>(R.id.buttonE),
+            findViewById<Button>(R.id.buttonU),
+            findViewById<Button>(R.id.buttonSIN),
+            findViewById<Button>(R.id.buttonDEG),
+            findViewById<Button>(R.id.buttonAC),
+            findViewById<Button>(R.id.buttonBCK),
+            findViewById<Button>(R.id.buttonDIV),
+            findViewById<Button>(R.id.buttonX),
+            findViewById<Button>(R.id.button7),
+            findViewById<Button>(R.id.button8),
+            findViewById<Button>(R.id.button9),
+            findViewById<Button>(R.id.buttonMINUS),
+            findViewById<Button>(R.id.button4),
+            findViewById<Button>(R.id.button5),
+            findViewById<Button>(R.id.button6),
+            findViewById<Button>(R.id.button1),
+            findViewById<Button>(R.id.button2),
+            findViewById<Button>(R.id.button3),
+            findViewById<Button>(R.id.button0),
+            findViewById<Button>(R.id.buttonPOINT),
+            findViewById<Button>(R.id.buttonPLUS),
+            findViewById<Button>(R.id.buttonEQUAL)
+        )
+        val textColor = resources.getColor(R.color.my_text_color, null)
 
         for (button in buttons) {
+            button.setTextColor(textColor)
+        }
+        for (button in buttons) {
             button.setOnClickListener {
-                resultTxt.text = button.text
+                if (button.id == R.id.buttonEQUAL) {
+                    stepsTxt.text = stringBuilder.toString()
+                    stringBuilder.clear()
+                    outputTxt.text =""
+                } else {
+                    val buttonText = button.text.toString().substringAfter("button")
+                    stringBuilder.append(buttonText)
+                    outputTxt.text = stringBuilder.toString()
+                }
             }
         }
     }
